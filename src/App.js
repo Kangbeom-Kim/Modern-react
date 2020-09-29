3a1b342b360026dc81d7e9c2b9f9341795a0689c
kangbeom <kkb931115@gmail.com>
2020-09-29 13:54:48 +0900

    useRef로 컴포넌트 안의 변수 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import UserList from './UserList';
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    },
+    {
+      id: 4,
+      username: 'custom',
+      email: 'custom@example.com'
+    },
+  ];
+
+  const nextId = useRef(5);
+  const onCreate = () => {
+    nextId.current += 1;
+  }
   return (
     <>
-      <UserList/>
+      <UserList users={users}/>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -5,24 +5,7 @@ function User({ user }) {
         <div><b>{user.name}</b><span>({user.email})</span></div>
     );
 }
-export default function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
+export default function UserList({ users }) {
 
     return (
         <div>
