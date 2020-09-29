8ccdbdd532f866dd5486b8d0b9032b50770eff10
kangbeom <kkb931115@gmail.com>
2020-09-29 11:22:21 +0900

    함수형 업데이트를 이용한 카운터 state 값 업데이트

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 
+/*
+    이전 방법은 state 를 새로운 값으로 대체하였지만,
+    이 방법을 사용하면 기존 state 값을 업데이트 하는 것.
+    후자의 방법은 컴포넌트 성능 최적화에 사용된다.
+*/
 export default function Counter() {
     const [num, HandleNum] = useState(0);
     const onPlusNum = () => {
-        HandleNum(num + 1);
+        HandleNum(prevNum => prevNum + 1);
     }
     const onMinusNum = () => {
-        HandleNum(num - 1);
+        HandleNum(prevNum => prevNum - 1);
     }
     return(
         <div>
