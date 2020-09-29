36429403418a54853487442508b36e266b25a2c0
kangbeom <kkb931115@gmail.com>
2020-09-29 10:41:27 +0900

    defaultProps 로 기본값 설정

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Hello 
-        Content={name}
+        // Content={name}
         ColorOfFont={style.color}
       />
       <div style={style}>{name}</div>
--- src/Hello.js
+++ src/Hello.js
@@ -3,3 +3,7 @@ import React from 'react';
 export default function Hello({Content, ColorOfFont}) {
     return <div style={{ color: ColorOfFont }}> 안녕하세요, {Content}!</div>
 }
+
+Hello.defaultProps = {
+    Content: '콘텐츠 없음'
+}
