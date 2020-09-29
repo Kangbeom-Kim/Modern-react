a0b82e1759b1c7a8e96e4849332dd9c13d8d00d8
kangbeom <kkb931115@gmail.com>
2020-09-29 11:03:04 +0900

    조건부 렌더링 코드 개선(false 일 경우 아무것도 없을 때)

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         />
         <Hello ColorOfFont="pink"/>
       </Wrapper>
-     
+
     </>
   );
 }
--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Hello({Content, ColorOfFont, isSpecial }) {
     return (
         <div style={{ color: ColorOfFont }}> 
-            {isSpecial ? <b>*</b> : null }
+            {isSpecial && <b>*</b> }
             안녕하세요, {Content}!
         </div>
     );
