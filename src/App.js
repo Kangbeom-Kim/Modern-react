76463cf636c7ec3646cc4d9f5dd6d086723f5205
kangbeom <kkb931115@gmail.com>
2020-09-29 10:21:31 +0900

    jsx 에 스타일 추가하기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Hello from './Hello';
 function App() {
   const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 25,
+    padding: 5,
+  }
   return (
-    <div>
+    <>
       <Hello/>
-      <div>{name}</div>
-    </div>
+      <div style={style}>{name}</div>
+    </>
   );
 }
 
