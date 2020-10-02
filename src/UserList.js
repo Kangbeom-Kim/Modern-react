7da70e2b47b4e9ace113e045b0553b4745d014a7
HandsomeChoco <kkb931115@gmail.com>
2020-10-02 17:25:33 +0900

    배열에 항목 추가 및 삭제

--- src/UserList.js
+++ src/UserList.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove }) {
     return (
-        <div><b>{user.name}</b><span>({user.email})</span></div>
+        <>
+            <div>
+                <b>{user.username}</b>
+                <span>({user.email})</span>
+                <button onClick={() => onRemove(user.id)}>삭제</button>
+            </div>
+            
+        </>
     );
 }
-export default function UserList({ users }) {
+export default function UserList({ users, onRemove }) {
 
     return (
         <div>
-            {users.map((data, index) => {
-                return(
-                    <User user={data} key={index}/>
-                );
-            })}
+            {users.map((data, index) => (
+                <User user={data} key={index} onRemove={onRemove}/>
+            ))}
         </div>
     );
 }
