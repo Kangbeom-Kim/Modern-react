21b01d5fdcc584b9cb821c54c4a74d688612e45e
HandsomeChoco <kkb931115@gmail.com>
2020-10-04 01:28:05 +0900

    useReducer 를 사용한 state 관리 (카운터 예제)

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,26 @@
 import React, { useState } from 'react';
+import { useReducer } from 'react';
 
-/*
-    이전 방법은 state 를 새로운 값으로 대체하였지만,
-    이 방법을 사용하면 기존 state 값을 업데이트 하는 것.
-    후자의 방법은 컴포넌트 성능 최적화에 사용된다.
-*/
-export default function Counter() {
-    const [num, HandleNum] = useState(0);
-    const onPlusNum = () => {
-        HandleNum(prevNum => prevNum + 1);
+function reducer(state, action) {
+    switch(action.type) {
+        case 'PLUS':
+            return state + 1;
+        case 'MINUS':
+            return state - 1;
+        default:
+            return state;
     }
+}
+
+export default function Counter() {
+    const [num, dispatch] = useReducer(reducer, 0);
+
+    const onPlusNum = () => {
+        dispatch({ type: 'PLUS' });
+    };
+
     const onMinusNum = () => {
-        HandleNum(prevNum => prevNum - 1);
+        dispatch({ type: 'MINUS' });
     }
     return(
         <div>
