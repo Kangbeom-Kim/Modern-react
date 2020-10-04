54bbf62a795e12f497c4af7232785ef8899615b2
HandsomeChoco <kkb931115@gmail.com>
2020-10-04 18:26:25 +0900

    custom hook 을 만들어 onChange, reset 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useRef, useCallback, useReducer } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import useInputs from './hooks/useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...');
@@ -41,17 +42,7 @@ const initialState = {
 }
 
 function reducer(state, action) {
-  console.log(action);
-  console.log(state)
   switch (action.type) {
-    case 'CHANGE_INPUT': 
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
     case 'CREATE_USER': 
       return {
         inputs: initialState.inputs,
@@ -75,20 +66,14 @@ function reducer(state, action) {
 }
 
 function App() {
+  const [{ username, email }, onChange, reset ] = useInputs({
+    username: '',
+    email: ''
+  });
   const [ state, dispatch ] = useReducer(reducer, initialState);
   const nextId = useRef(4);
 
   const { users } = state;
-  const { username, email } = state.inputs;
-
-  const onChange = useCallback(e => {
-    const { name, value} = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value,
-    });
-  }, []);
 
   const onCreate = useCallback(() => {
     dispatch({
@@ -99,8 +84,9 @@ function App() {
         email
       }
     });
+    reset();
     nextId.current++;
-  }, [username, email]);
+  }, [username, email, reset]);
 
   const onToggle = useCallback(id => {
     dispatch({
