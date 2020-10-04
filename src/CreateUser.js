6b7efe43efd0497964e0ea1731a6961998a86243
HandsomeChoco <kkb931115@gmail.com>
2020-10-04 23:43:54 +0900

    onToggle, onRemove 이벤트 핸들러까지 ContextAPI 로 처리

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useContext, useCallback, useRef } from 'react';
+import { UserDispatch } from './App';
+import useInputs from './hooks/useInputs';
 
-function CreateUser({ username, email, onChange, onCreate }) {
+function CreateUser() {
     console.log('CreateUser component')
+    const [{username, email}, onChange, reset] = useInputs({
+        username: '',
+        email: ''
+    });
+
+    const nextId = useRef(5);
+    const dispatch = useContext(UserDispatch);
+
+    const onCreate = () => {
+        dispatch({
+          type: 'CREATE_USER',
+          user: {
+            id: nextId.current,
+            username, 
+            email
+          }
+        });
+        reset();
+        nextId.current++;
+    }
+
     return(
         <div>
-            <input name="username" placeholder="계정명" onChange={onChange} value={username}/>
-            <input name="email" placeholder="이메일" onChange={onChange} value={email}/>
-            <button onClick={onCreate}>등록</button>
+            <input 
+                name="username" 
+                placeholder="계정명"
+                value={username}
+                onChange={onChange} 
+            />
+            <input 
+                name="email" 
+                placeholder="이메일" 
+                value={email}
+                onChange={onChange}
+            />
+            <button 
+                onClick={onCreate}
+            >
+                등록
+            </button>
         </div>
     );
 }
